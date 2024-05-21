bbbd2944ff6fe795557c58637ebe7212d2b79bb1
Jacob Hollebon <jacob@hollebon.com>
2024-05-21 22:43:11 +0100

    Add realtime class and runner for object to HOS format encoding

--- hosrealtime/realtime/realtime_renderers/__init__.py
+++ hosrealtime/realtime/realtime_renderers/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from .realtime_object_to_hos_loudspeaker_renderer import RealtimeObjectToHOSLoudspeakerRenderer
+from .realtime_object_to_hos_encoder import RealtimeObjectToHOSEncoder
