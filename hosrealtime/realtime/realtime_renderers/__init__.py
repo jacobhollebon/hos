5545889d6e59fc1233778c212348a092def26d5d
Jacob Hollebon <jacob@hollebon.com>
2024-05-21 23:24:57 +0100

    Add realtime class and runner for HOS loudspeaker decoder

--- hosrealtime/realtime/realtime_renderers/__init__.py
+++ hosrealtime/realtime/realtime_renderers/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from .realtime_object_to_hos_loudspeaker_renderer import RealtimeObjectToHOSLoudspeakerRenderer
 from .realtime_object_to_hos_encoder import RealtimeObjectToHOSEncoder
+from .realtime_hos_loudspeaker_decoder import RealtimeHOSLoudspeakerDecoder
