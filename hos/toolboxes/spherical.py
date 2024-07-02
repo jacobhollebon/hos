7070cc1a8a897f700e9f310f3b92abbdde96e671
Jacob Hollebon <jacob@hollebon.com>
2024-07-03 00:00:22 +0100

    Add mag-ls iSHT function

--- hos/toolboxes/spherical.py
+++ hos/toolboxes/spherical.py
@@ -42,7 +42,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import spharpy
-import sofar
+import sofar    
+from scipy.signal import group_delay
+from statistics import median
 
     
 def Nkr( N, r=0.0875, c=343 ):
@@ -171,7 +173,7 @@ def sphHarm( pos, N, kind='realSN3D', plot=False ):
         Ynm /= normFactor
         return Ynm
     
-def iSHT( data, pos, N, kind='realsn3d', beta=1e-8):
+def iSHT( data, pos, N, kind='realsn3d', beta=1e-15):
     '''
     Calculate the inverse spherical harmonic transform of a set of data
     
@@ -242,12 +244,195 @@ def iSHT( data, pos, N, kind='realsn3d', beta=1e-8):
     elif dims == 3:
         dtype = (YnmInv @ data[:,0,:]).dtype
         data_nm = np.zeros((numCoeffs, data.shape[1], data.shape[2]), dtype=dtype)
-        for indx in range(data.shape[1]):
-            data_nm[:, indx, :] = YnmInv @ data[:, indx, :]
+        for ch in range(data.shape[1]):
+            data_nm[:, ch, :] = YnmInv @ data[:, ch, :]
     
     return Ynm, YnmInv, data_nm 
         
+def iSHTmagls( data, pos, N, kind='realsn3d', beta=1e-15, fmagls=None, fade=None, removeGD=False, fs=48000, NFFT=None):
+    '''
+    Calculate the inverse spherical harmonic transform of a set of data
+    
+    Transform to the spherical harmonic domain
+    
+    The transform is performed using the regularised pseudoinverse of the
+    SH matrix
+    
+    Above a certain frequency the magnitude least-squares algorithm is applied
+    which priorities the reproduction of the magntiude at the cost of an erranous
+    phase representations
+    
+    ### TODO
+    Add delay management
+    add NFFT arg for high f resoltuion when designing
+    
+    Parameters
+    ----------
+    data : Array-like, shape (Q, time/freq) or (Q, chs, time/freq)
+        The data to undergo the inverse spherical harmonic transform
+        May be supplied in the time or frequency domain
+        If all data is real assumed to be time domain
+    pos : Array-like, shape(Q, 3)
+        Q x spherical sampling positions with trailing dimension ordered 
+        azimuth (rads, 0 to 2pi)
+        elevation (rads, pi/2 to -pi/2)
+        radius (m, 0 to inf) 
+    N : int
+        Order of the sampling
+    kind : str, optional
+        The type of spherical harmonic, options are
+        complex: complex SHs 
+        realn3d: real with n3d normalisation
+        realsn3d: real with sn3d normalisation 
+    beta : float, optional
+        Tikhonov regularisation value 
+    fmagls : None or float, optional
+        Frequency in Hz above which to apply magls
+        If left as None, the N=kr frequency is used
+    fade : None or float, optional
+        If a float is supplied the LS and magLS solutions are faded between
+        over this many Hz starting from frequency fmagls Hz
+    removeGD : Bool, optional
+        If True, the group delay is estimated by synthesising the omni component
+        and removed on an ear-by-ear basis
+        This may help with the phase reconstruction
+    fs : float, optional
+        Sampling rate of the data in Hz
+    NFFT : None or float, optional
+        Value used for the fft dictating the frequency resolution
+        If None then the length of the time series (hrir in) or
+        current NFFT value (hrtf in) is used
+        May not be smaller either of this two values
+    
+        
+    Returns
+    -------
+    Ynm : Array-like, shape(Q, (N+1)**2)
+        Matrix of spherical harmonics up to order N sampled at the Q positions
+    YnmInv : Array-like, shape((N+1)**2, Q)
+        Regularised pseudoinversion of the spherical harmonics up to order N sampled at the Q positions
+    data_nm : Array-like, shape(N+1)**2, time/freq) or (N+1)**2, ch, time/freq))
+        The data in the spherical harmonic domain
+        The spherical harmonic coefficients which represent the supplied data
+        With magls applied
 
+    '''
+    
+    # Safety Checks
+    # check the kind of SH requested
+    kind = kind.lower()
+    supportedSHConventions = ["complex", "realn3d", "realsn3d"]
+    if kind not in supportedSHConventions:
+        raise ValueError(f"Invalid kind: Choose from {supportedSHConventions}")
+    # check the dimensions of the supplied data
+    dims = data.ndim
+    if dims > 3:
+        raise ValueError('Data larger than 3 dimensions is not supported')
+    elif dims < 2:
+        raise ValueError('Data should have at minimum 2 dimensions, positions x (time/freq)')
+    
+    # Test if the data is in time or freq domain
+    if not np.sum(np.imag(data)):
+        isReal = True
+        NFFTin = data.shape[-1]
+    else: 
+        isReal = False
+        NFFTin = (data.shape[-1]-1)*2
+    if NFFT is None:
+        NFFT = NFFTin
+    else:
+        if NFFT < NFFTin:
+            if isReal:
+                err = f'The NFFT supplied ({NFFT}) is smaller than the time series of the  supplied HRIR s({NFFTin} samples)'
+            else:
+                err = f'The NFFT supplied ({NFFT}) is smaller than the current NFFT of the supplied HRTFs ({NFFTin})'
+            raise ValueError(err)
+   
+    # Set up frequency variables and ensure data is in frequency domain
+    if isReal:
+        data = np.fft.rfft(data, NFFT, axis=-1) # magls requires freq domain
+    else:
+        if NFFT != NFFTin:
+            data = np.fft.irfft(data, NFFTin, axis=-1) # to time domain
+            data = np.fft.rfft(data, NFFT, axis=-1) # back to freq domain with new NFFT
+    f = np.linspace(0, fs/2, (NFFT//2)+1)
+    f_len = len(f)
+    
+    # Set up mag ls
+    if fmagls is None:
+        fmagls = Nkr(N)
+    magls_start = np.argmin(np.abs(f-fmagls)) # apply magls above this freq index
+    if fade is not None:
+        magls_stop  = np.argmin(np.abs(f-fmagls-fade))
+        fade_len = magls_stop-magls_start
+        if not (fade_len%2):
+            fade_len += 1 # ensure the window is odd to get a value of 1 sampled
+        fade = np.hanning(fade_len)
+        fade_in = fade[:(fade_len//2)+1]
+        fade_out = fade[(fade_len//2)+1:]
+    if magls_start > f_len:
+        raise ValueError(f'The requested magls start frequency {fmagls} Hz is larger than the nyquist frequencys {fs/2}!')
+    
+    # Compute the SH matrix 
+    Ynm = sphHarm( pos, N, kind=kind )
+    numCoeffs = (N+1)**2 # number of SHs
+    Q = pos.shape[0] # number of sampling positions
+    
+    # Regularised pseuodinversion of the SH matrix
+    YnmH = Ynm.conj().T
+    grammian = np.linalg.inv( (YnmH@Ynm) + beta*np.identity(numCoeffs) )
+    YnmInv =  grammian @ YnmH
+    
+    # Remove any group delay
+    if removeGD:
+        # Remove any delay from the HRIRs. This helps with the phase reconstrution in the magls section
+        # Synthesise the W SH channel for each ear, find the group delay based on this omni response
+        # Take the overall delay as the median of the group delay (should be consistent under diff Nfft sizes)
+        # Then rmove the delay
+        data = np.fft.irfft(data, NFFT, axis=-1) # needs time domain data
+        if dims == 2:
+            W = YnmInv[0,:] @ data
+            _, gd  = group_delay((W,  1), w=f, fs=fs)
+            data = np.roll(data, -round(median(gd)), axis=-1)
+        elif dims == 3:
+            for ch in range(data.shape[1]):
+               W = YnmInv[0,:] @ data[:, ch, :]
+               _, gd  = group_delay((W,  1), w=f, fs=fs)
+               data[:, ch, :] = np.roll(data[:, ch, :], -round(median(gd)), axis=-1)
+        data = np.fft.rfft(data, NFFT, axis=-1)
+    
+    # Perform the iSHT 
+    if dims == 2:
+        data_nm = YnmInv @ data
+    elif dims == 3:
+        dtype = (YnmInv[0,:] @ data[:,0,:]).dtype 
+        data_nm = np.zeros((numCoeffs, data.shape[1], data.shape[2]), dtype=dtype)
+        for ch in range(data.shape[1]):
+            data_nm[:, ch, :] = YnmInv @ data[:, ch, :]
+    
+    # Peform mag ls
+    data_nm_magls = data_nm.copy() 
+    for findx in range(magls_start, f_len):
+        if dims == 2:
+            phi = np.angle(Ynm @ data_nm[:, findx-1]) 
+            data_nm_magls[:, findx] = YnmInv @ (np.abs(data[:, findx])*np.exp(1j*phi))
+        elif dims == 3:
+            for ch in range(data.shape[1]):
+                phi = np.angle(Ynm @ data_nm[:, ch, findx-1]) 
+                data_nm_magls[:, ch, findx] = YnmInv @ (np.abs(data[:, ch, findx])*np.exp(1j*phi))
+    
+    # Join together to create the output
+    data_nm_out = data_nm.copy()
+    data_nm_out[..., magls_start:] = data_nm_magls[..., magls_start:]
+    if fade is not None:
+        data_out = data_nm[..., magls_start:magls_start+fade_len]*fade_out
+        data_in  = data_nm_magls[..., magls_start:magls_start+fade_len]*fade_in
+        data_nm_out[..., magls_start:magls_start+fade_len] =  data_out + data_in
+     
+    if isReal:
+        data_nm_out = np.fft.irfft(data_nm_out, NFFT, axis=-1) # return to time domain
+        
+    return Ynm, YnmInv, data_nm_out 
 
 def SHT( data_nm, pos, N, kind='realsn3d'):
     '''
@@ -309,10 +494,10 @@ def SHT( data_nm, pos, N, kind='realsn3d'):
         for indx in range(data.shape[1]):
             data[:, indx, :] = Ynm @ data_nm[:, indx, :]
             
-    return Ynm, data_nm 
+    return Ynm, data 
             
 
-def sofa2sh( sofaPath, N=None, kind='realsn3d', beta=1e-8 ):
+def sofa2sh( sofaPath, N=None, kind='realsn3d', beta=1e-15 ):
     '''
     Load a sofa file and perform the iSHT returning the 
     hrir SH coefficients
@@ -375,6 +560,88 @@ def sofa2sh( sofaPath, N=None, kind='realsn3d', beta=1e-8 ):
         N = Nmax
     
     Ynm, YnmInv, hrir_nm = iSHT( hrir, pos, N, kind=kind, beta=beta)
+    
+    return Ynm, YnmInv, hrir_nm, pos, fs 
+
+def sofa2shmagls( sofaPath, N=None, kind='realsn3d', beta=1e-15, fmagls=None, fade=None, removeGD=False, NFFT=None):
+    '''
+    Load a sofa file and perform the iSHT returning the 
+    hrir SH coefficients
+    
+    
+    Parameters
+    ----------
+    sofaPath : str
+        File path to the sofa file
+    N : int or None
+        Order of the sampling
+        If None, the highest maximum order as per the number of
+        sampling positions in the sofa file will be used
+    kind : str, optional
+        The type of spherical harmonic, options are
+        complex: complex SHs 
+        realn3d: real with n3d normalisation
+        realsn3d: real with sn3d normalisation 
+    beta : float, optional
+        Tikhonov regularisation value for the iSHT psueodinversion
+    fmagls : None or float, optional
+        Frequency in Hz above which to apply magls
+        If left as None, the N=kr frequency is used
+    fade : None or float, optional
+        If a float is supplied the LS and magLS solutions are faded between
+        over this many Hz starting from frequency fmagls Hz
+    removeGD : Bool, optional
+        If True, the group delay is estimated by synthesising the omni component
+        and removed on an ear-by-ear basis
+        This may help with the phase reconstruction
+    NFFT : None or float, optional
+        Value used for the fft dictating the frequency resolution
+        If None then the length of the time series (hrir in) or
+        current NFFT value (hrtf in) is used
+        May not be smaller either of this two values
+    
+        
+    Returns
+    -------
+    Ynm : Array-like, shape(Q, (N+1)**2)
+        Matrix of spherical harmonics up to order N sampled at the Q positions
+    YnmInv : Array-like, shape((N+1)**2, Q)
+        Regularised pseudoinversion of the spherical harmonics up to order N sampled at the Q positions
+    hrir_nm : Array-like, shape(N+1)**2, time/freq) or (N+1)**2, ch, time/freq))
+        The hrir spherical harmonic coefficients
+    pos : Array-like, shape(Q, 3)
+        Q x spherical sampling positions of the hrir with trailing dimension ordered 
+        azimuth (rads, 0 to 2pi)
+        elevation (rads, pi/2 to -pi/2)
+        radius (m, 0 to inf)  
+    fs : float
+        Sampling frequency of the hrir in Hz
+
+    '''
+    
+    
+    # Read in the sofa file and extract the relevation data
+    file = sofar.read_sofa(sofaPath)
+    hrir = file.Data_IR
+    fs = file.Data_SamplingRate
+    pos = file.SourcePosition # spherical positions [az,el,r] in [deg,deg,m]
+
+    az = np.deg2rad(pos[:,0])
+    el = np.deg2rad(pos[:,1])
+    r = pos[:,2]
+    # new sampling position array
+    pos = np.stack([az, el, r], axis=1)
+    
+    Nmax = int(np.floor(np.sqrt(pos.shape[0])-1))
+    if N is None:
+        print(f'No truncation order supplied, using Nmax={Nmax}')
+        N = Nmax
+    elif N > Nmax:
+        print('You have requested an order higher than possible with this sampling regime')
+        print(f'Truncating to maximum order allowed: {Nmax}')
+        N = Nmax
+    
+    Ynm, YnmInv, hrir_nm = iSHTmagls( hrir, pos, N, kind=kind, beta=beta, fmagls=fmagls, fade=fade, fs=fs, NFFT=NFFT)
     
     return Ynm, YnmInv, hrir_nm, pos, fs 
 
